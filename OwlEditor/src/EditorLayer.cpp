fea6f105c287eee482e5a4227ee8e3e220b8eb88
Nolan Guton <49118528+TsukiShinen@users.noreply.github.com>
2024-06-26 21:26:28 +0200

    🐛 Fix flicker when resize viewport

--- OwlEditor/src/EditorLayer.cpp
+++ OwlEditor/src/EditorLayer.cpp
@@ -39,7 +39,16 @@ namespace Owl
 
     void EditorLayer::OnUpdate(const DeltaTime pDeltaTime)
     {
-        OWL_PROFILE_FUNCTION();
+    	OWL_PROFILE_FUNCTION();
+    	
+    	if (FramebufferSpecification spec = m_Framebuffer->GetSpecification();
+    		m_ViewportSize.x > 0.0f && m_ViewportSize.y > 0.0f &&
+    		(spec.Width != m_ViewportSize.x || spec.Height != m_ViewportSize.y))
+    	{
+    		m_Framebuffer->Resize((uint32_t)m_ViewportSize.x, (uint32_t)m_ViewportSize.y);
+    		m_CameraController.OnResize(m_ViewportSize.x, m_ViewportSize.y);
+    	}
+    	
     	if (m_ViewportFocused)
 			m_CameraController.OnUpdate(pDeltaTime);
 
@@ -145,13 +154,7 @@ namespace Owl
 		Application::Get().GetImGuiLayer()->BlockEvents(!m_ViewportFocused || !m_ViewportHovered);
 
 		ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
-		if (m_ViewportSize != *((glm::vec2*)&viewportPanelSize) && viewportPanelSize.x > 0 && viewportPanelSize.y > 0)
-		{
-			m_Framebuffer->Resize((uint32_t)viewportPanelSize.x, (uint32_t)viewportPanelSize.y);
-			m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
-
-			m_CameraController.OnResize(viewportPanelSize.x, viewportPanelSize.y);
-		}
+    	m_ViewportSize = { viewportPanelSize.x, viewportPanelSize.y };
 		uint32_t textureID = m_Framebuffer->GetColorAttachmentRendererId();
 		ImGui::Image((void*)textureID, ImVec2{ m_ViewportSize.x, m_ViewportSize.y }, ImVec2{ 0, 1 }, ImVec2{ 1, 0 });
 		ImGui::End();
