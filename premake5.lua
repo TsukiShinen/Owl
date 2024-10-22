55e9f6cb32e507f1db769aa7659baa72218c6a80
TsukiShinen <49118528+TsukiShinen@users.noreply.github.com>
2024-10-22 21:44:01 +0200

    .

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,4 @@
+include "./vendor/premake/premake_customization/solution_items.lua"
 include "Dependencies.lua"
 
 workspace "Owl"
@@ -19,6 +20,7 @@ workspace "Owl"
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 group "Dependencies"
+	include "vendor/premake"
 	include "Owl/vendor/GLFW"
 	include "Owl/vendor/Glad"
 	include "Owl/vendor/ImGui"
