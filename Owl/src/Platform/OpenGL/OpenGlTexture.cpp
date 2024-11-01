25a84f45b729131dc5b56fd5c05428b13386b839
TsukiShinen <49118528+TsukiShinen@users.noreply.github.com>
2024-11-01 11:44:51 +0100

    .

--- Owl/src/Platform/OpenGL/OpenGlTexture.cpp
+++ Owl/src/Platform/OpenGL/OpenGlTexture.cpp
@@ -19,7 +19,7 @@ namespace Owl
 						   static_cast<GLsizei>(m_Height));
 
 		glTextureParameteri(m_RendererId, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTextureParameteri(m_RendererId, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTextureParameteri(m_RendererId, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		
 		glTextureParameteri(m_RendererId, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTextureParameteri(m_RendererId, GL_TEXTURE_WRAP_T, GL_REPEAT);
--- OwlEditor/Assets/Shaders/Texture.glsl
+++ OwlEditor/Assets/Shaders/Texture.glsl
@@ -17,20 +17,20 @@ struct VertexOutput
 {
 	vec4 Color;
 	vec2 TexCoord;
-	float TexIndex;
 	float TilingFactor;
 };
 
 layout (location = 0) out VertexOutput Output;
+layout (location = 3) out flat float v_TexIndex;
 layout (location = 4) out flat int v_EntityId;
 
 void main() 
 {
 	Output.Color = in_Color;
 	Output.TexCoord = in_TexCoord;
-	Output.TexIndex = in_TexIndex;
 	Output.TilingFactor = in_TilingFactor;
 	v_EntityId = in_EntityId;
+	v_TexIndex = in_TexIndex;
 
 	gl_Position = u_ViewProjection * vec4(in_Position, 1.0);
 }
@@ -45,11 +45,11 @@ struct VertexOutput
 {
 	vec4 Color;
 	vec2 TexCoord;
-	float TexIndex;
 	float TilingFactor;
 };
 
 layout (location = 0) in VertexOutput Input;
+layout (location = 3) in flat float v_TexIndex;
 layout (location = 4) in flat int v_EntityId;
 
 layout (binding = 0) uniform sampler2D u_Textures[32];
@@ -57,7 +57,7 @@ layout (binding = 0) uniform sampler2D u_Textures[32];
 void main() 
 {
 	vec4 texColor = Input.Color;
-	switch(int(Input.TexIndex))
+	switch(int(v_TexIndex))
 	{		
 		case  0: texColor *= texture(u_Textures[ 0], Input.TexCoord * Input.TilingFactor); break;
 		case  1: texColor *= texture(u_Textures[ 1], Input.TexCoord * Input.TilingFactor); break;
